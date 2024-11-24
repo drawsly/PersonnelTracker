67953d3541316e8266cdb07fa28d806f22e100be
Enes Yasin Divrengi <dr4wsly@gmail.com>
2024-11-24 13:26:18 +0300

    Added Organizasyon İslemleri Page but not started

---
+++ PersonnelTracker/ViewModels/Pages/OrganizasyonIslemleriViewModel.cs
@@ -0,0 +1,25 @@
+﻿using PersonnelTracker.Services;
+
+namespace PersonnelTracker.ViewModels.Pages
+{
+    public partial class OrganizasyonIslemleriViewModel : ObservableObject
+    {
+        private readonly INavigationService _navigationService;
+        private readonly GostergePaneliViewModel _gostergePaneliViewModel;
+        private readonly ISnackbarService _snackbarService;
+        private readonly IContentDialogService _contentDialogService;
+
+        public OrganizasyonIslemleriViewModel(
+            INavigationService navigationService,
+            GostergePaneliViewModel gostergePaneliViewModel,
+            ISnackbarService snackbarService,
+            IContentDialogService contentDialogService)
+        {
+            _navigationService = navigationService;
+            _gostergePaneliViewModel = gostergePaneliViewModel;
+            _snackbarService = snackbarService;
+            _contentDialogService = contentDialogService;
+
+        }
+    }
+}
---
+++ PersonnelTracker/Views/Pages/OrganizasyonIslemleriPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using PersonnelTracker.ViewModels.Pages;
+
+namespace PersonnelTracker.Views.Pages
+{
+    /// <summary>
+    /// Interaction logic for OrganizasyonIslemleriPage.xaml
+    /// </summary>
+    public partial class OrganizasyonIslemleriPage : INavigableView<OrganizasyonIslemleriViewModel>
+    {
+        public OrganizasyonIslemleriViewModel ViewModel { get; }
+
+        public OrganizasyonIslemleriPage(OrganizasyonIslemleriViewModel viewModel)
+        {
+            ViewModel = viewModel;
+            DataContext = viewModel;
+
+            InitializeComponent();
+        }
+    }
+}
